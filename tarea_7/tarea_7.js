c30b8a686a21efc3c2096c35bdee1fe71a22b9a5
franromeroe <franromeroeze@gmail.com>
2023-05-03 09:20:56 -0300

    task 7

---
+++ tarea_7/tarea_7.js
@@ -0,0 +1,14 @@
+const familia = [
+    'Fran',
+    'Fernando',
+    'Mary',
+    'Juan Carlos',
+    'Kati'
+];
+const mySet = new Set(familia);
+
+mySet.add('Fran');
+console.log(mySet);
+
+mySet.add('JavaScript');
+console.log(mySet);
