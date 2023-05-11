fd859e2f3708d407410e0ac9c27805d8d5cce85b
franromeroe <franromeroeze@gmail.com>
2023-05-11 09:35:08 -0300

    listas

---
+++ guia_ejercicios/ARRAY/ej-7.js
@@ -0,0 +1,6 @@
+// Crea una lista con los nombres de tus 3 deportes favoritos y luego agrega
+// otro deporte al final de la lista.
+
+sports = ['football', 'bascketball', 'gym']
+sports.push('beisball');
+console.log(sports)
---
+++ guia_ejercicios/ARRAY/ej-8.js
@@ -0,0 +1,7 @@
+// Crea una lista con los nombres de tus 5 libros favoritos y luego imprime solo
+// los primeros 3 libros de la lista.
+
+let books = ['book1', 'book2', 'book3', 'book4', 'book5'];
+
+console.log(books[0], books[1], books[2]) // book1 book2 book3
+console.log(books.slice(0,3)) // ['book1', 'book2', 'book3']
