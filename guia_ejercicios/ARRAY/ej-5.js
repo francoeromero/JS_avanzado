0c63ab7b4160e93bf6e34d9cb0980fb76e9df043
franromeroe <franromeroeze@gmail.com>
2023-05-11 09:09:44 -0300

    listas

---
+++ guia_ejercicios/ARRAY/ej-5.js
@@ -0,0 +1,9 @@
+// Crea una lista con los nombres de 5 ciudades y luego imprime el último
+// elemento de la lista.
+
+let cities = ['Buenos aires', 'Madrid', 'New York', 'California'];
+
+console.log(cities.length) // 4 // son 4 elementos
+console.log(cities[cities.length -1]) // le resto 1, para nombrar la posicion // California
+console.log(cities.pop()) // California
+
---
+++ guia_ejercicios/ARRAY/ej-6.js
@@ -0,0 +1,6 @@
+// Crea una lista con 3 números enteros y luego agrega un nuevo número al final
+// de la lista.
+
+let numeros = [3, 4, 5]
+numeros.push(6)
+console.log(numeros)
