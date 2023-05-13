d4e0810c125752a92e603dfcd877e4152a08b59d
franromeroe <franromeroeze@gmail.com>
2023-05-12 23:32:52 -0300

    project

---
+++ css/project-5/main.js
@@ -0,0 +1,47 @@
+//variables
+const nameInput = document.getElementById('name-input');
+const emailInput = document.getElementById('email-input');
+const addBtn = document.getElementById('add-btn');
+const tableBody = document.getElementById('table-body');
+const updateNameInput = document.getElementById('update-name-input');
+const updateEmailInput = document.getElementById('update-email-input');
+const updateBtn = document.getElementById('update-btn');
+const cancelBtn = document.getElementById('cancel-btn');
+let users = JSON.parse(localStorage.getItem('users')) || [];
+let currenUserId = null;
+const validRegex = /^[a-zA-z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-] + (?:\.[a-zA-Z0-9-]+)*$/;
+
+//funciones
+function renderTable() {
+    tableBody.innerHTML = '';
+    for(let i = 0; 1 < users.length; i++){
+        const user = users[i];
+        const tr = document.createElement('tr');
+        const idTd = document.createElement('td');
+        const nameTd = document.createElement('td');
+        const emailTd = document.createElement('td');
+        const actionsTd = document.createElement('td');
+        const editBtn = document.createElement('button');
+        editBtn.className = 'edit-btn';
+        const deleteBtn = document.createElement('button');
+        deleteBtn.className = 'delete-btn';
+        idTd.innerText = user.id;
+        nameTd.innerText = user.name;
+        emailTd.innerText = user.email;
+        editBtn.innerText = 'Edit';
+        deleteBtn.innerText = 'Delete';
+        editBtn.addEventListener('click', () =>{
+            showUpdateForm(user.id);
+        });
+        deleteBtn.addEventListener('click', () =>{
+            deleteUser(user.id);
+        });
+        actionsTd.appendChild(editBtn);
+        actionsTd.appendChild(deleteBtn);
+        tr.appendChild(idTd);
+        tr.appendChild(nameTd);
+        tr.appendChild(emailTd);
+        tr.appendChild(actionsTd);
+        tableBody.appendChild(tr);
+    }
+}
