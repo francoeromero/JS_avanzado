5dc4010eba2daef8a43f4b011871d96666eb125d
franromeroe <franromeroeze@gmail.com>
2023-05-13 14:22:03 -0300

    project

--- css/project-5/main.js
+++ css/project-5/main.js
@@ -56,7 +56,41 @@ function addUser() {
             if(name && email != null){
                 var id = 1;
                 var val = users.map(function(x){return x.id;}).indexOf(id);
+                while(val != -1){
+                    id++;
+                    val = users.map(function(x){return x.id; }).indexOf(id);
+                }
+                const user = {
+                    id: id,
+                    name: name,
+                    email: email,
+                };
+                users.push(user);
+                localStorage.setItem("users", JSON.stringify(users));
+                nameInput.value = "";
+                emailInput.value = "";
+                renderTable();
+            } else{
+                alert("Name is Required");
             }
+        }else {
+            alert("Invalid email address!")
         }
     }
 }
+    function updateUser() {
+        const name = updateNameInput.value;
+        const email = updateEmailInput.value;
+        if(email.match(validRegex)){
+            const index = users.findIndex((user) => user.id === currenUserId);
+            if(index !== -1){
+                users[index].name = name;
+                users[index].email = email;
+                localStorage.setItem("users", JSON.stringify(users));
+                hideUpdateForm();
+                renderTable();
+            }
+        }else{
+            alert("Invalid email address! ");
+        }
+    }
