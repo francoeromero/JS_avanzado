8e6f5d6bd4d60882017d3cf1731ce3521233ee2b
franromeroe <franromeroeze@gmail.com>
2023-05-23 15:57:22 -0300

    project

---
+++ css/project-7/app.js
@@ -0,0 +1,12 @@
+// carrusel interactivo
+const heroSwiper = new Swiper(
+    // selecciono el elemento contenedor de las imagenes
+    ".hero__swiper",
+    {
+        pagination:{
+            el: ".swiper-pagination",
+            clickable: true
+        }
+    }
+);
+// carrusel interactivo
