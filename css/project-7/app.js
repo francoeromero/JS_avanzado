c85d978874ee7224a697d78424c3881363674c87
franromeroe <franromeroeze@gmail.com>
2023-05-23 18:50:36 -0300

    project

--- css/project-7/app.js
+++ css/project-7/app.js
@@ -10,3 +10,16 @@ const heroSwiper = new Swiper(
     }
 );
 // carrusel interactivo
+
+const topProductSwiper = new Swiper(".top-product__swiper",{
+    slidesPerView: 1,
+    spaceBetween: 30,
+    breakpoints: {
+        600: {
+            slidesPerView: 2
+        },
+        768: {
+            slidesPerView: 3
+        }
+    }
+})
