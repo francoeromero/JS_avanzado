525edb0b607261332cd3349089940aacdf112450
franromeroe <franromeroeze@gmail.com>
2023-05-29 17:36:27 -0300

    cronometro

--- css/project-3-Scroll-Cambio/main.js
+++
@@ -1,6 +0,0 @@
-const isScrolling = () => {
-    const headerEl = document.querySelector('.primary-header')
-    let windowPosition = window.scrollY > 250
-    headerEl.classList.toggle('active', windowPosition)
-}
-window.addEventListener('scroll', isScrolling)
